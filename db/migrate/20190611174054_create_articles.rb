04a784297e502825f25cc21f01cf91ecc9709f09
Richard LaFond <lafond6609@gmail.com>
2019-06-15 11:19:29 -0500

    More App Progress

--- db/migrate/20190611174054_create_articles.rb
+++ db/migrate/20190611174054_create_articles.rb
@@ -1,10 +1,10 @@
-class CreateArticles < ActiveRecord::Migration[5.2]
-  def change
-    create_table :articles do |t|
-      t.string :title
-      t.text :text
-
-      t.timestamps
+class CreateArticles < ActiveRecord::Migration[5.0]
+    def change
+        create_table :articles do |t|
+            t.string :title
+            t.text :text
+            
+            t.timestamps
+        end
     end
-  end
 end
