b2263defee15962814fd27e3be561c832df88131
Richard LaFond <lafond6609@gmail.com>
2019-06-17 13:56:41 -0500

    More Updates to App

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,13 +16,13 @@ class ArticlesController < ApplicationController
     end
     
     def create
-    @article = Article.new(article_params)
-    
-    if @article.save
-        redirect_to @article
-        else
-        render 'new'
-    end
+        @article = Article.new(article_params)
+        
+        if @article.save
+            redirect_to @article
+            else
+            render 'new'
+        end
     end
     
     def update
@@ -35,9 +35,15 @@ class ArticlesController < ApplicationController
         end
     end
     
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        
+        redirect_to articles_path
+    end
+    
     private
     def article_params
         params.require(:article).permit(:title, :text)
     end
-    end
-    
+end
